ed904d882df5bb33b8c29cf58a13d2cfe4893e2f
Thomas Witz <86101452+SchauT@users.noreply.github.com>
2024-01-06 17:56:35 +0100

    add: postUser function to api.ts

--- front-end/src/api.ts
+++ front-end/src/api.ts
@@ -2,11 +2,11 @@ import { env } from '$env/dynamic/public';
 
 export const getHealth = async () => {
     return await fetch(`${env.PUBLIC_API_URL}/health`).then((res) => res.json());
-}
+};
 
 export const getLateByUsers = async () => {
     return await fetch(`${env.PUBLIC_API_URL}/late`).then((res) => res.json());
-}
+};
 
 export const postLate = async (userId: string, startedAt: Date) => {
     return await fetch(`${env.PUBLIC_API_URL}/late`, {
@@ -16,8 +16,18 @@ export const postLate = async (userId: string, startedAt: Date) => {
         },
         body: JSON.stringify({ userId, startedAt })
     }).then((res) => res.json());
-}
+};
 
 export const fetchUser = async () => {
     return await fetch(`${env.PUBLIC_API_URL}/user`).then((res) => { console.log(res); return res.json() });
-}
+};
+
+export const postUser = async (user: any) => {
+    return await fetch(`${env.PUBLIC_API_URL}/user`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ user })
+    }).then((res) => res.json());
+};
